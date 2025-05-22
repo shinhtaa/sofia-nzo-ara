764cacac2b92673128c02771cbe9dcb95dfe1a23
shinhtaa <shintaauliapry@gmail.com>
2025-05-22 13:33:07 +0700

    SistemManajemenKeuangan.c

--- SistemManajemenKeuangan.c
+++ SistemManajemenKeuangan.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 int main () {
-
     int menu;
     int pemasukan, pengeluaran;
-    char keterangan [50];
+    char keterangan[50];
     int jumlahPemasukan = 0;
     int jumlahPengeluaran = 0;
+    char lanjut;
 
     do {
-        printf ("\nSistem Manajemen Keuangan Harian\n");
+        printf("\nSistem Manajemen Keuangan Harian\n");
         printf("1. Hitung Pemasukan\n");
         printf("2. Hitung Pengeluaran\n");
         printf("3. Lihat Ringkasan\n");
@@ -19,34 +19,41 @@ int main () {
         printf("\n");
 
         if (menu == 1) {
+            do {
                 printf("Silahkan masukkan nominal: ");
                 scanf("%d", &pemasukan);
                 printf("Silahkan masukkan keterangan: ");
-                scanf("%s", &keterangan);
-                printf("Pemasukan berhasil ditambahkan.");
-                printf("\n");
+                scanf("%s", keterangan);
                 jumlahPemasukan += pemasukan;
-                jumlahPemasukan++;
-        } else if (menu ==  2) {
+                printf("Pemasukan berhasil ditambahkan.\n");
+
+                printf("Ingin tambah lagi? (y/n): ");
+                scanf(" %c", &lanjut);
+            } while (lanjut == 'y' || lanjut == 'Y');
+
+        } else if (menu == 2) {
+            do {
                 printf("Silahkan masukkan nominal: ");
                 scanf("%d", &pengeluaran);
                 printf("Silahkan masukkan keterangan: ");
-                scanf("%s", &keterangan);
-                printf("Pengeluaran berhasil ditambahkan.");
-                printf("\n");
+                scanf("%s", keterangan);
                 jumlahPengeluaran += pengeluaran;
-                jumlahPengeluaran++;
+                printf("Pengeluaran berhasil ditambahkan.\n");
+
+                printf("Ingin tambah lagi? (y/n): ");
+                scanf(" %c", &lanjut);
+            } while (lanjut == 'y' || lanjut == 'Y');
+
         } else if (menu == 3) {
-                printf("Ringkasan Pemasukan dan Pengeluaran Hari Ini\n");
-                printf("Pemasukan: Rp. %d\n", jumlahPemasukan);
-                printf("Pengeluaran: Rp. %d\n", jumlahPengeluaran);
-                printf("\n");
-        } else if (menu < 1) {
-            printf("Menu tidak tersedia. Silahkan coba lagi.\n");
-        } else if (menu > 4) {
+            printf("Ringkasan Pemasukan dan Pengeluaran Hari Ini\n");
+            printf("Pemasukan: Rp. %d\n", jumlahPemasukan);
+            printf("Pengeluaran: Rp. %d\n", jumlahPengeluaran);
+            printf("\n");
+        } else if (menu < 1 || menu > 4) {
             printf("Menu tidak tersedia. Silahkan coba lagi.\n");
         }
 
     } while (menu != 4);
 
+    return 0;
 }
